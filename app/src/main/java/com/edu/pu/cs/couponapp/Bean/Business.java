6effb3ff7301749e285229251646478d2a186d5a
hatake77 <843122034@qq.com>
2019-05-26 21:04:47 +0800

    新增coupon business类

---
+++ app/src/main/java/com/edu/pu/cs/couponapp/Bean/Business.java
@@ -0,0 +1,31 @@
+package com.edu.pu.cs.couponapp.Bean;
+
+public class Business {
+    private String abbreviation;
+    private String title;
+    private String titlelogo;
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitlelogo() {
+        return titlelogo;
+    }
+
+    public void setTitlelogo(String titlelogo) {
+        this.titlelogo = titlelogo;
+    }
+}
